9e701803c8dd22a211d66433f0a2de2a3052e7d1
bhavinbhuta <bhuta.bhavin1994@gmail.com>
2017-06-26 20:44:51 -0400

    Add files via upload

---
+++ WrappedIOException.java
@@ -0,0 +1,18 @@
+
+import java.io.IOException;
+
+
+public class WrappedIOException {
+
+	public static IOException wrap(final Throwable e) {
+		return wrap(e.getMessage(), e);
+	}
+
+	public static IOException wrap(final String message, final Throwable e) {
+		final IOException wrappedException = new IOException(message + " ["
+				+ e.getMessage() + "]");
+		wrappedException.setStackTrace(e.getStackTrace());
+		wrappedException.initCause(e);
+		return wrappedException;
+	}
+}
